b83ff9c29afa2439740b086dd9c612dc3358a0ab
Jiaye-DF <jiaye.he@df-recycle.com>
2026-04-09 17:53:26 +0800

    (AI) Add: 白名單加入 df-sso-management.apps.zerozero.tw

--- backend/migrations/1744193100000_seed-test-env.js
+++ backend/migrations/1744193100000_seed-test-env.js
@@ -4,13 +4,14 @@ exports.up = (pgm) => {
   pgm.sql(`
     INSERT INTO sso_allowed_list (domain, name, env, description)
     VALUES
-      ('https://df-sso-login.apps.zerozero.tw', 'SSO Frontend', 'test', 'DF-SSO Login (Test)')
+      ('https://df-sso-login.apps.zerozero.tw', 'SSO Login', 'test', 'DF-SSO Login (Test)'),
+      ('https://df-sso-management.apps.zerozero.tw', 'SSO Management', 'test', 'DF-SSO 管理後台 (Test)')
     ON CONFLICT DO NOTHING
   `);
 };
 
 exports.down = (pgm) => {
   pgm.sql(`
-    DELETE FROM sso_allowed_list WHERE domain = 'https://df-sso-login.apps.zerozero.tw'
+    DELETE FROM sso_allowed_list WHERE domain IN ('https://df-sso-login.apps.zerozero.tw', 'https://df-sso-management.apps.zerozero.tw')
   `);
 };
